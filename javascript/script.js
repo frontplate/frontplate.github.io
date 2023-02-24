c5a66359a0fc0e9482e13852cc6d7be48403abdb
ung-bi <117797776+ung-bi@users.noreply.github.com>
2023-02-24 16:01:33 +0900

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -103,7 +103,7 @@ const s_s_byDate_listBtn = document.querySelector('.s_s_byDate_listBtn');
 title.innerHTML = 'ooooongb'
 
 // 블로그 이름
-h_blogName.innerHTML = "<button style = 'font-weight: bold'>ooooongb</button>"
+h_blogName.innerHTML = "<button>ooooongb</button>"
 
 // 모든 글 페이지
 if(allPostPage !== null){
